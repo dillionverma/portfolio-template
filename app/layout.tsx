cb0050194ab15e8c72d2974a47f0452e56ddb5c6
Dillion Verma <hello@dillion.io>
2024-05-08 19:22:15 -0400

    fix: small build fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,11 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
 export const metadata: Metadata = {
   title: "Developer Portfolio",
   description: "My developer portfolio",
--- app/page.tsx
+++ app/page.tsx
@@ -123,7 +123,7 @@ const projects = [
 
 export default function Component() {
   return (
-    <main className="flex flex-col min-h-[100dvh]">
+    <main className="flex flex-col min-h-[100dvh] divide-y">
       <section className="w-full py-12 md:py-24 lg:py-32 ">
         <div className="container px-4 md:px-6 grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
           <div className="flex flex-col justify-center space-y-4">
@@ -238,10 +238,7 @@ export default function Component() {
 
       <section id="projects"></section>
 
-      <section
-        className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800"
-        id="projects"
-      >
+      <section className="w-full py-12 md:py-24 lg:py-32" id="projects">
         <div className="container space-y-12 px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
